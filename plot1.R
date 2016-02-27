d78053ebeb12a7a65b37c7ab35d47dfd5e1cef6a
gabrielverta <gabrielverta@gmail.com>
2016-02-26 22:55:01 -0300

    Plot 1

---
+++ plot1.R
@@ -0,0 +1,20 @@
+# Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? Using the base plotting system, make a plot showing the total PM2.5 emission from all sources for each of the years 1999, 2002, 2005, and 2008.
+library(dplyr)
+
+pm25Data <- function(){
+  readRDS("summarySCC_PM25.rds")
+}
+
+parseNumber <- function(num){
+  paste(sprintf("%.1f", num / 1000000), "M", sep = "")
+}
+
+plot1 <- function(data){
+    group <- group_by(data, year)
+    data <- summarise(group, total=sum(Emissions))
+    yrng <- range(data$total)
+    ylabels <- c(parseNumber(yrng[1]), parseNumber(yrng[2]))
+    plot(data, pch=19, xlab="Year", ylab="PM2.5 Emission", xlim=c(1999, 2008), yaxt="n")
+    axis(2, at=yrng, labels=ylabels)
+    lines(response, lwd = 2, col="red")
+}
